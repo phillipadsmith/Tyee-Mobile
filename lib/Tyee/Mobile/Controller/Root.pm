f6b32775e22949c05e758027b01f3b1ff5f3bd96
Phillip Smith <ps@phillipadsmith.com>
2011-10-29 19:04:03 -0400

    Added contact page, seperated styles.css, added text sizing, etc.

--- lib/Tyee/Mobile/Controller/Root.pm
+++ lib/Tyee/Mobile/Controller/Root.pm
@@ -11,7 +11,7 @@ BEGIN { extends 'Catalyst::Controller' }
 # Sets the actions in this controller to be registered with no prefix
 # so they function identically to actions created in MyApp.pm
 #
-__PACKAGE__->config(namespace => '');
+__PACKAGE__->config( namespace => '' );
 
 =head1 NAME
 
@@ -29,12 +29,12 @@ The root page (/)
 
 =cut
 
-sub index :Path :Args(0) {
+sub index : Path : Args(0) {
     my ( $self, $c ) = @_;
     my $sections = $c->config->{'sections'};
     my $topics   = $c->config->{'topics'};
     $c->stash(
-        results => $c->model('API')->lookup_latest_grouped(),
+        results  => $c->model( 'API' )->lookup_latest_grouped(),
         sections => $sections,
         topics   => $topics,
         template => 'index.tt',
@@ -43,31 +43,48 @@ sub index :Path :Args(0) {
 
 # Story URIs are /Section/YYYY/MM/DD/Slug/
 # TODO Change to a RegEx capture
-sub story :Path :Args(5) {
-    my ( $self, $c, $section, $year, $month, $day, $slug ) = @_;
-    my $path = join('/', $section, $year, $month, $day, $slug);
-    my $story = $c->model('API')->lookup_story( $path );
-    my $now       = DateTime->now();
-    my $date_obj  = DateTime::Format::ISO8601->parse_datetime( $story->{'storyDate'} );
-    my $date_str  = DateTime->compare( $now, $date_obj) ? 'Today' : join(' ', $date_obj->day, $date_obj->month_abbr, $date_obj->year);
+sub story : Path : Args(5) {
+    my ( $self, $c, $section_path, $year, $month, $day, $slug ) = @_;
+    my $path  = join( '/', $section_path, $year, $month, $day, $slug );
+    my $size  = $c->req->param( 'size' ); # Text size
+    my $story = $c->model( 'API' )->lookup_story( $path );
+    my $now   = DateTime->now();
+    my $date_obj
+        = DateTime::Format::ISO8601->parse_datetime( $story->{'storyDate'} );
+
+# Compare two DateTime objects. The semantics are compatible with Perl's sort() function;
+# it returns -1 if $dt1 < $dt2, 0 if $dt1 == $dt2, 1 if $dt1 > $dt2.
+    my $today = DateTime->compare( $now, $date_obj );
+    my $date_str
+        = $today == 0
+        ? 'Today'
+        : join( ' ', $date_obj->day, $date_obj->month_abbr, $date_obj->year );
+
+    # Return the first matching element of a search on the url attribute
+    # across the sections array. Used to properly display section names
+    # in the section overview pages.
+    my $sections = $c->config->{'sections'};
+    my $section = first { $_->{url} =~ $section_path } @$sections;
     $c->stash(
-        #story => $c->model('API')->lookup_story( $path ),
         story    => $story,
         date     => $date_str,
         path     => $path,
+        section  => $section,
+        size     => $size,
         template => 'article.tt'
     );
 }
 
-
 # Blog URIs are /Blogs/TheHook/Media/2011/09/08/Tyee-App-Canadian-Magazine/
 # TODO Change this to a RegEx capture
-sub blog :Path :Args(7) {
-    my ( $self, $c, $blogs, $blog, $section, $year, $month, $day, $slug ) = @_;
-    my $path = join('/', $blogs, $blog, $section, $year, $month, $day, $slug);
+sub blog : Path : Args(7) {
+    my ( $self, $c, $blogs, $blog, $section, $year, $month, $day, $slug )
+        = @_;
+    my $path
+        = join( '/', $blogs, $blog, $section, $year, $month, $day, $slug );
     $c->stash(
-        story => $c->model('API')->lookup_story( $path ),
-        path  => $path,
+        story    => $c->model( 'API' )->lookup_story( $path ),
+        path     => $path,
         template => 'blog.tt'
     );
 }
@@ -75,30 +92,35 @@ sub blog :Path :Args(7) {
 # /(Section name) (list latest 20 from section name)
 # TODO Change this to a RegEx capture
 # Need to capture the section URL and convert to section name lookup
-sub section :Path :Args(1) {
+sub section : Path : Args(1) {
     my ( $self, $c, $section_path ) = @_;
+
+    # Return the first matching element of a search on the url attribute
+    # across the sections array. Used to properly display section names
+    # in the section overview pages.
     my $sections = $c->config->{'sections'};
     my $section = first { $_->{url} =~ $section_path } @$sections;
     $c->stash(
-        results => $c->model('API')->lookup_topic( $section->{'name'} ),
-        title   => $section->{'name'},
+        results  => $c->model( 'API' )->lookup_topic( $section->{'name'} ),
+        title    => $section->{'name'},
         template => 'section.tt',
-    )
+    );
 }
 
 # /Topcics (list all sections & Topics)
 
-sub topic :Path :Args(2) {
+sub topic : Path : Args(2) {
     my ( $self, $c, $stub, $topic_path ) = @_;
+
     # TODO
     # Check if $topic is TheHook. If so, detach to sub blogs
     my $topics = $c->config->{'topics'};
     my $topic = first { $_->{url} =~ $topic_path } @$topics;
     $c->stash(
-        results => $c->model('API')->lookup_topic( $topic->{'name'} ),
-        title   => $topic->{'name'},
+        results  => $c->model( 'API' )->lookup_topic( $topic->{'name'} ),
+        title    => $topic->{'name'},
         template => 'section.tt'
-    )
+    );
 }
 
 # TODO
@@ -106,15 +128,21 @@ sub topic :Path :Args(2) {
 #
 #sub blogs {}
 
-
-sub search :Regex('^search$') {
+sub search : Regex('^search$') {
     my ( $self, $c ) = @_;
-    my $query = $c->req->param('query');
+    my $query = $c->req->param( 'query' );
     $c->stash(
-        results => $c->model('API')->lookup_query( $query ),
-        title   => $query,
+        results  => $c->model( 'API' )->lookup_query( $query ),
+        title    => $query,
         template => 'section.tt'
-    )
+    );
+}
+
+sub contact : Path('/contact') : Args(0) {
+    my ( $self, $c ) = @_;
+    $c->stash(
+        template => 'contact.tt'
+    );
 }
 
 =head2 default
@@ -123,10 +151,10 @@ Standard 404 error page
 
 =cut
 
-sub default :Path {
+sub default : Path {
     my ( $self, $c ) = @_;
     $c->response->body( 'Page not found' );
-    $c->response->status(404);
+    $c->response->status( 404 );
 }
 
 =head2 end
@@ -135,7 +163,8 @@ Attempt to render a view, if needed.
 
 =cut
 
-sub end : ActionClass('RenderView') {}
+sub end : ActionClass('RenderView') {
+}
 
 =head1 AUTHOR
 
